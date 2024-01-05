75bcd0590248201a3e548b9e2f18aa8fb5855caa
chengtongxue666 <75940911+chengtongxue666@users.noreply.github.com>
2024-01-05 23:04:03 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -136,6 +136,7 @@ export const FooterImpl: React.FC = () => {
           </a>
         )}
       </div>
+      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=1876411759&auto=1&height=32"></iframe>
     </footer>
   )
 }
